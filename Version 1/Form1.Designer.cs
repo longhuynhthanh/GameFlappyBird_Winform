b87621ecf12847fc9d9f5cd1eeface65638446f8
LongHT <38778007+longhuynhthanh@users.noreply.github.com>
2019-05-31 21:07:29 +0700

    upload source

---
+++ Version 1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Version_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OngTren1 = new System.Windows.Forms.PictureBox();
+            this.OngDuoi1 = new System.Windows.Forms.PictureBox();
+            this.OngTren2 = new System.Windows.Forms.PictureBox();
+            this.OngDuoi2 = new System.Windows.Forms.PictureBox();
+            this.ConChim = new System.Windows.Forms.PictureBox();
+            this.NutPlay = new System.Windows.Forms.PictureBox();
+            this.lblDiemSo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.OngTren1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngDuoi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngTren2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngDuoi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConChim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NutPlay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OngTren1
+            // 
+            this.OngTren1.Image = ((System.Drawing.Image)(resources.GetObject("OngTren1.Image")));
+            this.OngTren1.Location = new System.Drawing.Point(319, -706);
+            this.OngTren1.Name = "OngTren1";
+            this.OngTren1.Size = new System.Drawing.Size(276, 1002);
+            this.OngTren1.TabIndex = 0;
+            this.OngTren1.TabStop = false;
+            // 
+            // OngDuoi1
+            // 
+            this.OngDuoi1.Image = ((System.Drawing.Image)(resources.GetObject("OngDuoi1.Image")));
+            this.OngDuoi1.Location = new System.Drawing.Point(319, 378);
+            this.OngDuoi1.Name = "OngDuoi1";
+            this.OngDuoi1.Size = new System.Drawing.Size(276, 1001);
+            this.OngDuoi1.TabIndex = 1;
+            this.OngDuoi1.TabStop = false;
+            // 
+            // OngTren2
+            // 
+            this.OngTren2.Image = ((System.Drawing.Image)(resources.GetObject("OngTren2.Image")));
+            this.OngTren2.Location = new System.Drawing.Point(791, -716);
+            this.OngTren2.Name = "OngTren2";
+            this.OngTren2.Size = new System.Drawing.Size(276, 1002);
+            this.OngTren2.TabIndex = 2;
+            this.OngTren2.TabStop = false;
+            // 
+            // OngDuoi2
+            // 
+            this.OngDuoi2.Image = ((System.Drawing.Image)(resources.GetObject("OngDuoi2.Image")));
+            this.OngDuoi2.Location = new System.Drawing.Point(791, 378);
+            this.OngDuoi2.Name = "OngDuoi2";
+            this.OngDuoi2.Size = new System.Drawing.Size(276, 1001);
+            this.OngDuoi2.TabIndex = 3;
+            this.OngDuoi2.TabStop = false;
+            // 
+            // ConChim
+            // 
+            this.ConChim.Image = ((System.Drawing.Image)(resources.GetObject("ConChim.Image")));
+            this.ConChim.Location = new System.Drawing.Point(66, 358);
+            this.ConChim.Name = "ConChim";
+            this.ConChim.Size = new System.Drawing.Size(128, 101);
+            this.ConChim.TabIndex = 4;
+            this.ConChim.TabStop = false;
+            // 
+            // NutPlay
+            // 
+            this.NutPlay.Image = ((System.Drawing.Image)(resources.GetObject("NutPlay.Image")));
+            this.NutPlay.Location = new System.Drawing.Point(-1, 1);
+            this.NutPlay.Name = "NutPlay";
+            this.NutPlay.Size = new System.Drawing.Size(51, 50);
+            this.NutPlay.TabIndex = 5;
+            this.NutPlay.TabStop = false;
+            this.NutPlay.Click += new System.EventHandler(this.NutPlay_Click);
+            // 
+            // lblDiemSo
+            // 
+            this.lblDiemSo.AutoSize = true;
+            this.lblDiemSo.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiemSo.Location = new System.Drawing.Point(1033, 1);
+            this.lblDiemSo.Name = "lblDiemSo";
+            this.lblDiemSo.Size = new System.Drawing.Size(88, 31);
+            this.lblDiemSo.TabIndex = 6;
+            this.lblDiemSo.Text = "Score:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1190, 584);
+            this.Controls.Add(this.lblDiemSo);
+            this.Controls.Add(this.NutPlay);
+            this.Controls.Add(this.ConChim);
+            this.Controls.Add(this.OngDuoi2);
+            this.Controls.Add(this.OngTren2);
+            this.Controls.Add(this.OngDuoi1);
+            this.Controls.Add(this.OngTren1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.OngTren1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngDuoi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngTren2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngDuoi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConChim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NutPlay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox OngTren1;
+        private System.Windows.Forms.PictureBox OngDuoi1;
+        private System.Windows.Forms.PictureBox OngTren2;
+        private System.Windows.Forms.PictureBox OngDuoi2;
+        private System.Windows.Forms.PictureBox ConChim;
+        private System.Windows.Forms.PictureBox NutPlay;
+        private System.Windows.Forms.Label lblDiemSo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Version 1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int DoChenhLechGiuaHaiOng = 300;
+        int X_Cap1, x_Cap2;
+
+        int y_OngTren1, y_OngTren2;
+        int y_OngDuoi1, y_OngDuoi2;
+
+        int DiemSo = 0;
+        int Dem = 0;
+
+        private void NutPlay_Click(object sender, EventArgs e)
+        {
+            Dem++;
+            if (Dem % 2 != 0)
+            {
+                timer1.Start();
+                timer2.Start();
+            }
+            else
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // DoubleBuffer => hiệu chỉnh không bị giựt màn hình.
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            if (y_ConChim + ConChim.Height <= this.Height)
+            {
+                y_ConChim += 10;
+                ConChim.Location = new Point(x_ConChim, y_ConChim);
+                if (x_ConChim + ConChim.Width >= X_Cap1 && x_ConChim + ConChim.Width <= X_Cap1 + OngTren1.Width)
+                {
+                    if (y_ConChim <= 1000 + y_OngTren1 || y_ConChim + ConChim.Height >= y_OngDuoi1)
+                    {
+                        timer1.Stop();
+                    }
+                }
+
+                if (x_ConChim + ConChim.Width >= x_Cap2 && x_ConChim + ConChim.Width <= x_Cap2 + OngTren2.Width)
+                {
+                    if (y_ConChim <= 1000 + y_OngTren2 || y_ConChim + ConChim.Height >= y_OngDuoi2)
+                    {
+                        timer1.Stop();
+                    }
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                y_ConChim -= 50; 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // DoubleBuffer => hiệu chỉnh không bị giựt màn hình.
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            X_Cap1 -= 10;
+            x_Cap2 -= 10;
+
+            OngTren1.Location = new Point(X_Cap1, y_OngTren1);
+            OngDuoi1.Location = new Point(X_Cap1, y_OngDuoi1);
+
+            OngTren2.Location = new Point(x_Cap2, y_OngTren2);
+            OngDuoi2.Location = new Point(x_Cap2, y_OngDuoi2);
+
+            // Kiểm tra nếu cặp ống 1 mà đi qua form thì cho nó hiện lại đằng sau ống hai.
+            if (X_Cap1 + OngTren1.Width <= 0)
+            {
+                DiemSo++;
+
+                // Hiện lại đằng sau cặp ống hai:
+                X_Cap1 = 700 + OngTren2.Width + 500;
+                // ramdom độ cao của hai ống cặp 1 cho đặc sắc hơn
+                Random rd = new Random();
+                y_OngTren1 = rd.Next(-800, -400);
+                y_OngDuoi1 = (1000 + y_OngTren1) + DoChenhLechGiuaHaiOng;
+
+                OngTren1.Location = new Point(X_Cap1, y_OngTren1);
+                OngDuoi1.Location = new Point(X_Cap1, y_OngDuoi1);
+            }
+
+            // Kiểm tra nếu cặp ống hai mà đi qua khỏi form thì cho nó hiện lại đằng sau ống 1:
+            if (x_Cap2 + OngTren2.Width <= 0)
+            {
+                DiemSo++;
+                x_Cap2 = 700 + OngTren1.Width + 500;
+                // random độ cao của hai ống cặp 2 cho đặc sắc hơn
+                Random rd = new Random();
+                y_OngTren2 = rd.Next(-800, -400);
+                y_OngDuoi2 = (1000 + y_OngTren2) + DoChenhLechGiuaHaiOng;
+
+                OngTren2.Location = new Point(x_Cap2, y_OngTren2);
+                OngDuoi2.Location = new Point(x_Cap2, y_OngDuoi2);
+            }
+            lblDiemSo.Text = "Score: " + DiemSo.ToString();
+
+        }
+
+        int x_ConChim, y_ConChim;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* CODE FULL MÀN HÌNH */
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Left = 0;
+            this.Top = 0;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            // Canh tọa độ khi vào game:
+
+            X_Cap1 = this.Width + 300; // ẩn các ống vào 300 để khi hiện ra màn hình sẽ không thấy các ống
+            y_OngTren1 = -700; // Cho độ cao của ống trên là 1000 thì khi đặt -700 thì ống trên sẽ chỉ hiển thị là 300 trên màn hình.
+            OngTren1.Size = new Size(270, 1000);
+            OngTren1.Location = new Point(X_Cap1, y_OngTren1);
+
+            // Đã có độ cao ống trên 1, độ chênh lệch giữa hai ống thì ta sẽ suy ra được độ cao ống thứ hai.
+
+            y_OngDuoi1 = (1000 + y_OngTren1) + DoChenhLechGiuaHaiOng;
+            OngDuoi1.Size = new Size(270, 1000);
+            OngDuoi1.Location = new Point(X_Cap1, y_OngDuoi1);
+
+            x_Cap2 = X_Cap1 + OngTren1.Width + 500; /******/
+            y_OngTren2 = -700;
+            OngTren2.Size = new Size(270, 1000);
+            OngTren2.Location = new Point(x_Cap2, y_OngTren2);
+
+            y_OngDuoi2 = (1000 + y_OngTren2) + DoChenhLechGiuaHaiOng;
+            OngDuoi2.Size = new Size(270, 1000);
+            OngDuoi2.Location = new Point(x_Cap2, y_OngDuoi2);
+
+            ConChim.Size = new Size(127, 100);
+            x_ConChim = ConChim.Location.X;
+            y_ConChim = ConChim.Location.Y;
+
+            NutPlay.Size = new Size(50, 50);
+
+            lblDiemSo.Location = new Point(this.Height - 100);
+            timer1.Interval = 1;
+            timer2.Interval = 50;
+        }
+    }
+}
